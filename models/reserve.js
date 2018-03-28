02064136eb0739754d2be8ae4e8b2296344140ac
jojoshare <396854595@qq.com>
2018-03-28 10:41:37 +0800

    8

--- models/reserve.js
+++ models/reserve.js
@@ -1,15 +1,30 @@
 'use strict'; 
 var mongoose = require('mongoose');  
 var reserveSchema = new mongoose.Schema({
-    userid: String,                                                                                                                         
-     name: String,                                                                                                                           
-     mobile: String,
-     mobileM: String,
-     mobileF: String,
-     time: Number,
-     nationcode: String,
-     text: String,
-     sign: String 
+    "﻿userid" : Number,
+    "type" : String,
+    "name" : String,
+    "sex" : String,
+    "birthday" : String,
+    "ID" : String,
+    "idAddress" : String,
+    "liveAddress" : String,
+    "school" : String,
+    "area" : String,
+    "nameM" : String,
+    "mobileM" : String,
+    "workplaceM" : String,
+    "educationM" : String,
+    "nameF" : String,
+    "mobileF" : String,
+    "workplaceF" : String,
+    "educationF" : String,
+    //回复
+    "mobile": String, 
+    "time": Number,
+    "nationcode": String,
+    "text": String,
+    "sign": String 
 }, {
     collection: 'reserve'
 });

2a8e59bb1e22b173ac3de4856bd5322e7f45b359
jojoshare <396854595@qq.com>
2018-03-30 09:25:06 +0800

    15

--- models/reserve.js
+++ models/reserve.js
@@ -1,30 +1,32 @@
 'use strict'; 
 var mongoose = require('mongoose');  
-var reserveSchema = new mongoose.Schema({
-    ﻿userid : String,
-    type : String,
-    name : String,
-    sex : String,
-    birthday : String,
-    ID : String,
-    idAddress : String,
-    liveAddress : String,
-    school : String,
-    area : String,
-    nameM : String,
-    mobileM : Number,
-    workplaceM : String,
-    educationM : String,
-    nameF : String,
-    mobileF : Number,
-    workplaceF : String,
-    educationF : String,
+var reserveSchema = new mongoose.Schema({    
+    seq : Number,
+    ﻿userid : String,//编号
+    num : Number,//序号
+    type : String,//1、升学类别
+    name : String,//2、学生姓名
+    sex : String,// 3、学生性别
+    birthday : String,//4、学生出生日期
+    ID : String,//5、学生身份证或居住证号码
+    idAddress : String,//6、户口地址
+    liveAddress : String,//7、家庭地址
+    school : String,// 8、现就读学校名称
+    area : String,//9、就读学校所在区
+    nameM : String,//10、母亲姓名
+    mobileM : Number,//11、母亲移动电话
+    workplaceM : String,//12、母亲工作单位
+    educationM : String,//13、母亲学历
+    nameF : String,//14、父亲姓名
+    mobileF : Number,//15、父亲移动电话
+    workplaceF : String,//16、父亲工作单位
+    educationF : String,//17、父亲学历
     hasCome:{
         type:String,
         default:"N"
     },
     //回复
-    mobile: String, 
+    mobile: String, //电话清单
     time: Number,
     nationcode: String,
     text: String,
